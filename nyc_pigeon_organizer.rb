c0aa5be4b71cec75f004b4667a240b89c3bb1eeb
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-07-21 18:24:52 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  listed_by_name = {}
+  data.each do |atributes, value| # color/gender etc and its value
+  value.each do |desription, names| # grey/male and its value
+  names.each do |name| #array of names
+  if listed_by_name.has_key?(name)
+    if listed_by_name[name].has_key?(atributes)
+      listed_by_name[name][atributes] << desription.to_s
+    else
+      listed_by_name[name][atributes] = [desription.to_s]
+    end
+    else
+    listed_by_name[name] = {atributes => [desription.to_s]}
+  end
+end
+end
+end
+return listed_by_name
 end
