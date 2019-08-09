6703fce00d0ffcb994bc9d85992aecf1d6d702b2
Angus Morrison <contact@angus-morrison.com>
2019-08-09 16:56:12 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,17 +5,19 @@ def nyc_pigeon_organizer(data)
   data.each_pair do | property, variant |
     variant.each_pair do | variant, names |
       names.each do | name |
+        
         #If the name doesn't already exist in the reorganized hash, add the name key with an empty hash value
         unless reorganized.has_key?(name)
           reorganized[name] = {}
         end
         
-        #Check whether the current name has the current property key, and push/create the variant value accordingly
-        if reorganized[name][property]
-          reorganized[name][property] << variant.to_s
-        else
-          reorganized[name][property] = [variant.to_s]
+        #Check whether the current name has the current property key
+        unless reorganized[name].has_key?(property)
+          reorganized[name][property] = []
         end
+
+        #Append the variant to the property array
+        reorganized[name][property] << variant.to_s
         
       end
     end
