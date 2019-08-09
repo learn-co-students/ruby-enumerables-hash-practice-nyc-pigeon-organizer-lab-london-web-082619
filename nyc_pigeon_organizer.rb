074c7566ba6e1b9f060e5a934c97a0044a0fba63
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-08-09 17:22:57 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require"pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, specific_attribute|
+    specific_attribute.each do |second_attribute, name|
+     name.each do |pigeon_name|
+       #binding.pry
+    if !pigeon_list[pigeon_name]
+      pigeon_list[pigeon_name] = {}
+    end
+    if !pigeon_list[pigeon_name][attribute]
+      pigeon_list[pigeon_name][attribute] = []
+    end
+    pigeon_list[pigeon_name][attribute] << second_attribute.to_s
+   end
+    end
+  end
+  #binding.pry
+  pigeon_list
 end
