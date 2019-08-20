418efb584d675a49429c4f4d38a99354f2e65391
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-08-20 16:04:00 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,24 @@
-require 'pry'
+#require 'pry'
+#binding.pry
 
 def nyc_pigeon_organizer(data)
-  hash = {}
-binding.pry
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
 
-  # rite your code here!
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+
+        new_hash[name][key] << new_value.to_s
+
+      end
+    end
+  end
+  new_hash
 end
