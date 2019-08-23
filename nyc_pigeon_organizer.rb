bf2b6241ab7d8bef375b2d7dce8c048009f3759d
Andrius Jurgsaitis <andriusj6@hotmail.com>
2019-08-23 09:14:30 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  seven_pigeons = {}
+  data.each do |key, value|
+    value.each do |value, name|
+      name.each do |pigeon|
+        seven_pigeons[pigeon] ||= {}
+        seven_pigeons[pigeon][key] ||= []
+        seven_pigeons[pigeon][key] << value.to_s
+      end
+    end    
+  end
+  seven_pigeons
 end
